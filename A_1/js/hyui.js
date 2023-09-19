889b16d3bd399e73e0a796c6cf99b0621a9bf18e
mingshuya <mingshuya@gmail.com>
2023-09-19 15:38:40 +0800

    20230919 js調整

--- A_1/js/hyui.js
+++ A_1/js/hyui.js
@@ -44,7 +44,7 @@ $(function() {
     $('header .container').prepend('<button type="button" class="sidebarCtrl">側欄選單</button><button type="button" class="searchCtrl">查詢</button>');
     $('header .container .sidebarCtrl').after('<aside class="sidebar"><div class="m_area"><button type="button" class="sidebarClose">關閉</button></div><div class="menu_overlay"></div></aside>');
     // $('#aC').prependTo('.moj_link .col+.col');
-    $('.innerpage').find('#aC').prependTo('.main_menu~.content');
+    // $('.innerpage').find('#aC').prependTo('.main_menu~.content');
 
     var menu_status = false;
     var _sidebar = $('.sidebar'),
